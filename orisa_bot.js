847946518b98f6b5146f48f453837c5819dca389
Victor Yun <Victor Yun>
2017-03-18 23:29:13 -0700

    added other routes

--- orisa_bot.js
+++ orisa_bot.js
@@ -32,11 +32,69 @@ controller.hears(['stop harassing me'], 'ambient', function(bot, message){
         }
         else {
           console.log("Got text:", res.messages[1].text);
+          bot.api.users.info({
+               user: res.messages[1].user
+            }, function(err, usrd){
+              dm.say('*User:* '+usrd.user.real_name);
+
+              dm.addMessage({
+                  text: 'Please list them below:',
+                  action: 'description_thread'
+              },'yes_thread');
+
+              dm.addMessage({
+                  text: 'That is fine.',
+                  action: 'description_thread'
+              },'no_thread');
+
+              
+              dm.addMessage({
+                  text: 'Sorry I did not understand.',
+                  action: 'default',
+              },'bad_response');
+
+              dm.addMessage({
+                  text: 'Thank you. Here are some links regarding workplace harassment: https://www.worksafebc.com/en/health-safety/hazards-exposures/bullying-harassment'
+
+              }, 'links_thread')
+
+              dm.addMessage({
+                  text: 'Please provide a short description of the incident: ',
+                  action: 'links_thread'
+              }, 'description_thread');
+
+
+              // Create a yes/no question in the default thread...
+              dm.ask('Were there any possible witnesses? (yes/no)', [
+                  {
+                      pattern: 'yes',
+                      callback: function(response, dm) {
+                          dm.gotoThread('yes_thread');
+                      },
+                  },
+                  {
+                      pattern: 'no',
+                      callback: function(response, dm) {
+                          dm.gotoThread('no_thread');
+                      },
+                  },
+                  {
+                      default: true,
+                      callback: function(response, dm) {
+                          dm.gotoThread('bad_response');
+                      },
+                  }
+              ]);
+          });
           dm.say('*Message:* '+res.messages[1].text);
-          dm.say('*User:* '+res.messages[1].user);
+          //dm.say('*User:* '+res.messages[1].user);
           dm.say('*Timestamp:* '+res.messages[1].ts);
-        }
+
+
+        }//end 
+
     });
+
 
   });
 });
