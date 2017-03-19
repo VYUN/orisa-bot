cba199caf48875de2ada8542d1dc3a11e28c604c
Miguel Fernandez <Miguel Fernandez>
2017-03-18 18:19:10 -0700

    deleted stuff

--- orisa_bot.js
+++ orisa_bot.js
@@ -144,20 +144,4 @@ controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your na
 
     });
 
-function formatUptime(uptime) {
-    var unit = 'second';
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'minute';
-    }
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'hour';
-    }
-    if (uptime != 1) {
-        unit = unit + 's';
-    }
 
-    uptime = uptime + ' ' + unit;
-    return uptime;
-}
